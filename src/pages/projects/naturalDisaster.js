b17f0ad67b62eb44e8bb3e2c8f0aed9960439d7f
MrCleen <54378358+GabiAnderson@users.noreply.github.com>
2023-10-02 22:31:45 -0600

    imported all photos for natty d

--- src/pages/projects/naturalDisaster.js
+++ src/pages/projects/naturalDisaster.js
@@ -27,11 +27,55 @@ import TextFormat from '@/components/Common/TextFormat';
 import { rotateInVariant, scaleInVariant } from '@/animations/ScrollAnimationVariants';
 
 // imports for media
-import HeaderImg from "@/images/projects/naturalDisaster/ND_header.png";
+import MEAE_Hompage from "@/images/projects/naturalDisaster/MEAE_HomepagePartial.png";
+import ArcadeLightingNew from "@/images/projects/naturalDisaster/ND_ArcadeLighting_4_4.png";
+import BannerArt1 from "@/images/projects/naturalDisaster/ND_BannerArt_1.png"
+import BannerArt2 from "@/images/projects/naturalDisaster/ND_BannerArt_2.png"
+// TODO image of banner art 3
+import BannerGif from "@/images/projects/naturalDisaster/ND_BannerUnroll.gif"
+import TRexBreak_Concept from "@/images/projects/naturalDisaster/ND_BreakableTRexMachinimaConcept_11_28.gif";
+import CarryableHook_Concept from "@/images/projects/naturalDisaster/ND_CarryableFishhookLineConcept_1_29.gif";
+import CarryableVFX_Concept from "@/images/projects/naturalDisaster/ND_CarryableVFXConcept_1_29.gif";
+import CarryObject_Concept from "@/images/projects/naturalDisaster/ND_CarryConcept_10_22.gif";
+import FerrisWheelRoll from "@/images/projects/naturalDisaster/ND_FerrisWheelRoll_4_4.gif";
+import LightingPerformanceTest from "@/images/projects/naturalDisaster/ND_LightingPerformanceComparison_2_7.gif";
+import LiveCharacterView from "@/images/projects/naturalDisaster/ND_LiveCharacterChangeView_Concept_3_23.png";
+import LoadingGif from "@/images/projects/naturalDisaster/ND_LoadingIcon_4_4.gif";
+import LoadingGifFinal from "@/images/projects/naturalDisaster/ND_LoadingIconFinal.gif";
+import MachinimaPipelineAssignment from "@/images/projects/naturalDisaster/ND_MachinimaPipeline_3_22.png";
 import MachinimaPipeline from "@/images/projects/naturalDisaster/ND_MachinimaPipeline.png";
-import ScannerCode from "@/images/projects/naturalDisaster/ND_ScannerConcept_Code_1_14.png";
-import ArcadeLight from "@/images/projects/naturalDisaster/ND_ArcadeLighting_4_4.png";
-import testGif from "@/images/projects/naturalDisaster/ND_LoadingIconFinal.gif"
+import MouseControl_Concept from "@/images/projects/naturalDisaster/ND_MouseControlConcept_9_20.gif";
+import MuseumLightingNew from "@/images/projects/naturalDisaster/ND_MuseumLighting_4_4.png";
+import ParkLightingNew from "@/images/projects/naturalDisaster/ND_ParkLighting_4_4.png";
+import PathCompleteUI_Concept from "@/images/projects/naturalDisaster/ND_PathCompleteUIConecpt_4_20.png";
+import PierLightingNew from "@/images/projects/naturalDisaster/ND_PierLighting_4_4.png";
+import PlayableMenu_Concept from "@/images/projects/naturalDisaster/ND_PlayableMainMenuConcept_1_19.gif";
+import PrisonBigWizard_Concept from "@/images/projects/naturalDisaster/ND_PrisonBigWizardConcept_4_5.gif";
+import PrisonLightingNew from "@/images/projects/naturalDisaster/ND_PrisonLighting_4_4.png";
+import Scanner_ConceptCode from "@/images/projects/naturalDisaster/ND_ScannerConcept_Code_1_14.png";
+import Scanner_Concept from "@/images/projects/naturalDisaster/ND_ScannerMachinimaVFXConcept_2_8.gif";
+import ScrapbookUI_Concept from "@/images/projects/naturalDisaster/ND_ScrapbookPage_Lawrence_11_28.png";
+import SemesterTimeline from "@/images/projects/naturalDisaster/ND_SecondSemesterTimeline.png";
+import SettingsMenu_UI from "@/images/projects/naturalDisaster/ND_SettingsMenu_4_24.png";
+import SteamHeader from "@/images/projects/naturalDisaster/ND_SteamHeader.png";
+import TeamPhoto from "@/images/projects/naturalDisaster/ND_TeamPhoto_12_8.png";
+import TheaterLightingNew from "@/images/projects/naturalDisaster/ND_TheaterLighting_4_4.png";
+import TrainLightingNew from "@/images/projects/naturalDisaster/ND_TrainLighting_4_4.png";
+import SceneChange_Concept from "@/images/projects/naturalDisaster/ND_TriggerSceneChangeConcept_10_7.gif";
+import TutorialUIOld from "@/images/projects/naturalDisaster/ND_Tutorial_4_11.png";
+import TutorialUI1 from "@/images/projects/naturalDisaster/ND_Tutorial_4_11.png";
+import TutorialUI2 from "@/images/projects/naturalDisaster/ND_Tutorial_4_22_2.png";
+import TutorialUI3 from "@/images/projects/naturalDisaster/ND_Tutorial_4_22_3.png";
+import TutorialUI4 from "@/images/projects/naturalDisaster/ND_Tutorial_4_22_4.png";
+import TutorialUI5 from "@/images/projects/naturalDisaster/ND_Tutorial_4_22_5.png";
+import WizardVFXConcept from "@/images/projects/naturalDisaster/ND_WizardVFXConcept_4_16.gif";
+import ArcadeLightingOld from "@/images/projects/naturalDisaster/ArcadeLightingOld.png";
+import MuseumLightingOld from "@/images/projects/naturalDisaster/MuseumLightingOld.png";
+import PierLightingOld from "@/images/projects/naturalDisaster/PierLightingOld.png";
+import PrisonLightingOld from "@/images/projects/naturalDisaster/PrisonLightingOld.png";
+import TheaterLighting from "@/images/projects/naturalDisaster/TheaterLightingOld.png";
+import ParkLightingOld from "@/images/projects/naturalDisaster/ParkLightingOld.png";
+import TrainLightingOld from "@/images/projects/naturalDisaster/TrainLightingOld.png";
 
 const naturalDisaster = () => {
     return (
@@ -85,6 +129,18 @@ const naturalDisaster = () => {
           <SectionTitle
             sectionTitle="Lead Engineer"
             sectionDescription=""
+          />
+
+          <HoverTripleColumn 
+            img1={testImg}
+            img2={testImg}
+            img3={testImg}
+            title1="HoverTripleColumn"
+            title2="HoverTripleColumn"
+            title3="HoverTripleColumn"
+            desc1="Desc 1"
+            desc2="Desc 2"
+            desc3="Desc 3"
           />
 
         </main>
