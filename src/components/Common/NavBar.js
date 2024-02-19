0a7cbe5f8bdcceb7ccfbc496465da9981c9c88f2
MrCleen <54378358+GabiAnderson@users.noreply.github.com>
2024-02-19 15:26:40 -0700

    made nav bar responsive

--- src/components/Common/NavBar.js
+++ src/components/Common/NavBar.js
@@ -1,5 +1,5 @@
 import Link from 'next/link'
-import React from 'react'
+import React, { useState } from 'react'
 import Logo from './Logo'
 import { useRouter } from 'next/router'
 import { TwitterIcon, GithubIcon, LinkedInIcon, DiscordIcon, SunIcon, MoonIcon } from './Icons'
@@ -18,53 +18,142 @@ const CustomLink = ({ href, title, className = '', underlineColor = 'black' }) =
         ${router.asPath === href ? 'w-full' : 'w-0'}`}
             >&nbsp;</span>
         </Link>
-    )
-}
+    );
+};
+
+const CustomLinkMobile = ({ href, title, className = '', underlineColor = 'black', toggle }) => {
+    const router = useRouter();
+
+    const handleClick = () => {
+        toggle();
+        router.push(href)
+    }
+
+    return (
+        <button href={href} className={`${className} relative group text-light dark:text-dark my-2`} onClick={handleClick}>
+            {title}
+            <span className={`h-[1px] inline-block bg-dark 
+        absolute left-0 -bottom-0.5 
+        group-hover:w-full transition-[width] ${underlineColor} ease duration-300
+        ${router.asPath === href ? 'w-full' : 'w-0'}`}
+            >&nbsp;</span>
+        </button>
+    );
+};
 
 const NavBar = () => {
     const [mode, setMode] = useThemeSwitcher();
 
+    const [isOpen, setIsOpen] = useState(false);
+
+    const handleHamburgerClick = () => {
+        setIsOpen(!isOpen)
+    }
+
     return (
-        <header className='w-fulle px-32 py-8 font-medium flex flex-col items-center justify-between dark:text-light'>
+        <header className='w-full px-32 py-8 font-medium flex flex-col items-center justify-between dark:text-light relative'>
             {/* First Row */}
             <nav className='flex items-center justify-between w-full mb-4'>
-                <div>
-                    <CustomLink href="/" title="Home" className='mr-4' underlineColor='bg-primaryColor'></CustomLink>
-                    <CustomLink href="/about" title="About" className='mx-4' underlineColor='bg-primaryColor'></CustomLink>
-                    <CustomLink href="/projects" title="Projects" className='mx-4' underlineColor='bg-primaryColor'></CustomLink>
-                    <CustomLink href="/articles" title="Articles" className='ml-4' underlineColor='bg-primaryColor'></CustomLink>
-                </div>
 
-                <div className='flex items-center'>
-                    <motion.a href="https://discord.com/users/488372834016690186" target={'_blank'}
-                        whileHover={{ y: -2 }}
-                        whileTap={{ scale: 0.9 }}
-                        className='w-6 mx-3'
-                    >
-                        <DiscordIcon className='dark:fill-light hover:fill-primaryColor dark:hover:fill-primaryColor' />
-                    </motion.a>
-                    <motion.a href="https://github.com/GabiAnderson" target={'_blank'}
-                        whileHover={{ y: -2 }}
-                        whileTap={{ scale: 0.9 }}
-                        className='w-6 mx-3'
-                    >
-                        <GithubIcon className='dark:fill-light hover:fill-primaryColor dark:hover:fill-primaryColor' />
-                    </motion.a>
-                    <motion.a href="https://linkedin.com/in/gabriela-anderson" target={'_blank'}
-                        whileHover={{ y: -2 }}
-                        whileTap={{ scale: 0.9 }}
-                        className='w-6 mx-3'
-                    >
-                        <LinkedInIcon className='dark:fill-light hover:fill-primaryColor dark:hover:fill-primaryColor' />
-                    </motion.a>
-                    <button
-                        onClick={() => setMode(mode === "light" ? "dark" : "light")}
-                        className={`ml-3 flex items-center justify-center rounded-full p-1 
-                        ${mode === "light" ? "bg-dark text-light" : "bg-light text-dark"}
-                        `}
-                    >
-                        {mode === "dark" ? <SunIcon className={"fill-dark"} /> : <MoonIcon className={"fill-dark"} />}
-                    </button>
+                {/* Hamburger menu button for smaller screens */}
+                <button className='flex-col justify-center items-center hidden lg:flex' onClick={handleHamburgerClick}>
+                    <span className={`bg-dark dark:bg-light block transition-all duration-300 ease-out h-0.5 w-6 rounded-sm ${isOpen ? 'rotate-45 translate-y-1' : '-translate-y-0.5'}`}></span>
+                    <span className={`bg-dark dark:bg-light block transition-all duration-300 ease-out h-0.5 w-6 rounded-sm my-0.5 ${isOpen ? 'opacity-0' : 'opacity-100'}`}></span>
+                    <span className={`bg-dark dark:bg-light block transition-all duration-300 ease-out h-0.5 w-6 rounded-sm ${isOpen ? '-rotate-45 -translate-y-1' : 'translate-y-0.5'}`}></span>
+                </button>
+
+                {/* Hamburger menu for smaller screens */}
+                {
+                    isOpen ?
+
+                    <motion.div
+                    initial={{scale:0, opacity:0, x:"-50%", y:"-50%"}}
+                    animate={{scale:1, opacity:1}}
+                    className='min-w-[70vw] flex flex-col justify-between z-30 items-center fixed top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/5
+                bg-dark/90 dark:bg-light/90 rounded-lg backdrop-blur-md py-32'>
+                        <div className='flex items-center flex-col justify-center'>
+                            <CustomLinkMobile href="/" title="Home" className='' underlineColor='bg-primaryColor' toggle={handleHamburgerClick} ></CustomLinkMobile>
+                            <CustomLinkMobile href="/about" title="About" className='' underlineColor='bg-primaryColor' toggle={handleHamburgerClick} ></CustomLinkMobile>
+                            <CustomLinkMobile href="/projects" title="Projects" className='' underlineColor='bg-primaryColor' toggle={handleHamburgerClick} ></CustomLinkMobile>
+                            <CustomLinkMobile href="/articles" title="Articles" className='' underlineColor='bg-primaryColor' toggle={handleHamburgerClick} ></CustomLinkMobile>
+                        </div>
+
+                        <div className='flex items-center justify-center flex-wrap-2'>
+                            <motion.a href="https://discord.com/users/488372834016690186" target={'_blank'}
+                                whileHover={{ y: -2 }}
+                                whileTap={{ scale: 0.9 }}
+                                className='w-6 mr-3 sm:mx-1'
+                            >
+                                <DiscordIcon className='fill-light dark:fill-dark hover:fill-primaryColor dark:hover:fill-primaryColor' />
+                            </motion.a>
+                            <motion.a href="https://github.com/GabiAnderson" target={'_blank'}
+                                whileHover={{ y: -2 }}
+                                whileTap={{ scale: 0.9 }}
+                                className='w-6 mx-3 sm:mx-1'
+                            >
+                                <GithubIcon className='fill-light dark:fill-dark hover:fill-primaryColor dark:hover:fill-primaryColor' />
+                            </motion.a>
+                            <motion.a href="https://linkedin.com/in/gabriela-anderson" target={'_blank'}
+                                whileHover={{ y: -2 }}
+                                whileTap={{ scale: 0.9 }}
+                                className='w-6 mx-3 sm:mx-1'
+                            >
+                                <LinkedInIcon className='fill-light dark:fill-dark hover:fill-primaryColor dark:hover:fill-primaryColor' />
+                            </motion.a>
+                            <button
+                                onClick={() => setMode(mode === "light" ? "dark" : "light")}
+                                className={`flex items-center justify-center rounded-full p-1 
+                                ${mode === "light" ? "bg-dark text-light" : "bg-light text-dark"}
+                                `}
+                            >
+                                {mode === "dark" ? <SunIcon className={"fill-dark"} /> : <MoonIcon className={"fill-dark"} />}
+                            </button>
+                        </div>
+                    </motion.div>
+
+                    : null
+                }
+
+                {/* Standard menu bar for regular screens */}
+                <div className='w-full flex justify-between items-center lg:hidden'>
+                    <div>
+                        <CustomLink href="/" title="Home" className='mr-4' underlineColor='bg-primaryColor'></CustomLink>
+                        <CustomLink href="/about" title="About" className='mx-4' underlineColor='bg-primaryColor'></CustomLink>
+                        <CustomLink href="/projects" title="Projects" className='mx-4' underlineColor='bg-primaryColor'></CustomLink>
+                        <CustomLink href="/articles" title="Articles" className='ml-4' underlineColor='bg-primaryColor'></CustomLink>
+                    </div>
+
+                    <div className='flex items-center'>
+                        <motion.a href="https://discord.com/users/488372834016690186" target={'_blank'}
+                            whileHover={{ y: -2 }}
+                            whileTap={{ scale: 0.9 }}
+                            className='w-6 mr-3'
+                        >
+                            <DiscordIcon className='dark:fill-light hover:fill-primaryColor dark:hover:fill-primaryColor' />
+                        </motion.a>
+                        <motion.a href="https://github.com/GabiAnderson" target={'_blank'}
+                            whileHover={{ y: -2 }}
+                            whileTap={{ scale: 0.9 }}
+                            className='w-6 mx-3'
+                        >
+                            <GithubIcon className='dark:fill-light hover:fill-primaryColor dark:hover:fill-primaryColor' />
+                        </motion.a>
+                        <motion.a href="https://linkedin.com/in/gabriela-anderson" target={'_blank'}
+                            whileHover={{ y: -2 }}
+                            whileTap={{ scale: 0.9 }}
+                            className='w-6 mx-3'
+                        >
+                            <LinkedInIcon className='dark:fill-light hover:fill-primaryColor dark:hover:fill-primaryColor' />
+                        </motion.a>
+                        <button
+                            onClick={() => setMode(mode === "light" ? "dark" : "light")}
+                            className={`ml-3 flex items-center justify-center rounded-full p-1 
+                            ${mode === "light" ? "bg-dark text-light" : "bg-light text-dark"}
+                            `}
+                        >
+                            {mode === "dark" ? <SunIcon className={"fill-dark"} /> : <MoonIcon className={"fill-dark"} />}
+                        </button>
+                    </div>
                 </div>
             </nav>
 
@@ -73,8 +162,8 @@ const NavBar = () => {
             </div>
 
             {/* Second Row */}
-            {/* TODO - just remove this nav when website is complete */}
-            <nav className='w-full flex items-center justify-center py-2'>
+            {/* TODO - to remove just add hidden to the end of the nav classname */}
+            <nav className='w-full flex items-center justify-center py-2 hidden'>
                 <div className='bg-yellow-500 text-center items-center p-4 rounded-lg' style={{ maxWidth: '1000px' }}>
                     <span className='text-6xl text-black font-extrabold'>!</span>
                     <h1 className='capitalize font-bold text-2xl mt-2'>Status Update</h1>
--- tailwind.config.js
+++ tailwind.config.js
@@ -60,6 +60,25 @@ module.exports = {
         circularDark: 'repeating-radial-gradient(rgba(91,154,139, 0.6) 2px, #0a0a0a 7px, #0a0a0a 100px);'
       }
     },
+    screens: {
+      "2xl": { max: "1535px" },
+      // => @ media (max-width: 1535px) { ... }
+
+      "xl": { max: "1279px" },
+      // => @ media (max-width: 1279px) { ... }
+
+      "lg": { max: "1023px" },
+      // => @ media (max-width: 1023px) { ... }
+
+      "md": { max: "767px" },
+      // => @ media (max-width: 767px) { ... }
+
+      "sm": { max: "639px" },
+      // => @ media (max-width: 639px) { ... }
+
+      "sx": { max: "479px" },
+      // => @ media (max-width: 479px) { ... }
+    }
   },
   plugins: [],
 }
