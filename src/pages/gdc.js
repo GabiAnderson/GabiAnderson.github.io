6dfefc9b0703973c8590582768904cbd463e1a3a
MrCleen <54378358+GabiAnderson@users.noreply.github.com>
2024-02-22 21:57:53 -0700

    added gdc landing page

---
+++ src/pages/gdc.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import Head from 'next/head'
+import Layout from '@/components/Common/Layout'
+import AnimatedText from '@/animations/AnimatedText'
+
+const articles =() => {
+    return (
+    <>
+        <Head>
+            <title>Gabi Anderson | GDC</title>
+            <meta name="description" content="any thing here" />
+        </Head>
+        <main className='w-full mb-16 flex flex-col items-center justify-center overflow-hidden'>
+            <Layout className='pt-16'>
+                <AnimatedText text="We met at GDC 2024!" className='mb-16 text-secondaryColor'/>
+            </Layout>
+        </main>
+    </>
+    )
+}
+
+export default articles
